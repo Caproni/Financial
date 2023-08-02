e2a75c123370ab87ae8b4adb8b3068e2134325b2
Caproni <bennettedmund.acc@gmail.com>
2023-08-02 17:06:41 +0100

    Corrected function

--- src/brokerage/alpaca/data/get_stock_bars.py
+++ src/brokerage/alpaca/data/get_stock_bars.py
@@ -7,6 +7,7 @@ Copyright 2023
 from alpaca.data.historical.stock import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.enums import Adjustment, DataFeed
+from alpaca.data.timeframe import TimeFrame
 from datetime import datetime
 
 from src.utils.logger import logger as log
@@ -15,15 +16,29 @@ from src.utils.logger import logger as log
 def get_stock_bars(
     client: StockHistoricalDataClient,
     symbols: list[str],
+    timeframe: TimeFrame,
     start: datetime = None,
     end: datetime = None,
-):
+) -> None:
+    """Gets historical stock data
+
+    Args:
+        client (StockHistoricalDataClient): An Alpaca data client.
+        symbols (list[str]): A list of symbols for which to obtain data.
+        timeframe (TimeFrame): The frequency of data to obtain.
+        start (datetime, optional): Start datetime for data retrieval. Defaults to None.
+        end (datetime, optional): End datetime for data retrieval. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
     log.info("Calling get_stock_bars")
-    client.get_stock_bars(
+    return client.get_stock_bars(
         StockBarsRequest(
             symbol_or_symbols=symbols,
             start=start,
             end=end,
+            timeframe=timeframe,
             adjustment=Adjustment.ALL,
         )
     )
