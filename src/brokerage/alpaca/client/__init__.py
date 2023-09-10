83ef3f915e7d7c5aa7cf068f67382c54ec768617
Edmund Bennett <bennettedmund@gmail.com>
2023-09-10 16:52:17 +0100

    Added clients creation to init

--- src/brokerage/alpaca/client/__init__.py
+++ src/brokerage/alpaca/client/__init__.py
@@ -3,3 +3,12 @@
 Author: Edmund Bennett
 Copyright 2023
 """
+
+from src.brokerage.alpaca.client.create_trading_client import create_trading_client
+from src.brokerage.alpaca.client.create_broker_client import create_broker_client
+from src.brokerage.alpaca.client.create_market_data_client import (
+    create_historical_stock_data_client,
+    create_live_stock_data_client,
+    create_historical_crypto_data_client,
+    create_live_crypto_data_client
+)
