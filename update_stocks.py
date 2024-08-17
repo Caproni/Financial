75a59a8a467b85fdbaaf7265c44f45bb61b3dc0b
Edmund Bennett <bennettedmund@gmail.com>
2024-08-17 18:53:53 +0100

    Update both hourly and daily datasets

--- update_stocks.py
+++ update_stocks.py
@@ -21,32 +21,38 @@ if __name__ == "__main__":
 
     log.info("Starting database update.")
 
-    timespan = "hour"
-    collection_name = f"polygon_market_data_{timespan}"
-
-    if collection_name == "polygon_market_data_day":
-        collection = PolygonMarketDataDay
-    elif collection_name == "polygon_market_data_hour":
-        collection = PolygonMarketDataHour
-
     database_client = create_sql_client()
 
-    latest_market_data_timestamps = get_data(
-        database_client,
-        models=[collection],
-        entities=[
-            collection.symbol,
-            func.max(collection.timestamp).label("timestamp"),
-        ],
-        group_by=[collection.symbol],
-    )
+    timespans = [
+        "day",
+        "hour",
+    ]
 
-    for latest_market_data_timestamp in latest_market_data_timestamps:
-        populate_database_latest_market_data(
-            timespan=timespan,
-            collection=collection,
-            ticker=latest_market_data_timestamp["symbol"],
-            from_=latest_market_data_timestamp["timestamp"] + timedelta(days=1),
+    for timespan in timespans:
+        collection_name = f"polygon_market_data_{timespan}"
+        log.info(f"Populating: {collection_name}")
+
+        if collection_name == "polygon_market_data_day":
+            collection = PolygonMarketDataDay
+        elif collection_name == "polygon_market_data_hour":
+            collection = PolygonMarketDataHour
+
+        latest_market_data_timestamps = get_data(
+            database_client,
+            models=[collection],
+            entities=[
+                collection.symbol,
+                func.max(collection.timestamp).label("timestamp"),
+            ],
+            group_by=[collection.symbol],
         )
+
+        for latest_market_data_timestamp in latest_market_data_timestamps:
+            populate_database_latest_market_data(
+                timespan=timespan,
+                collection=collection,
+                ticker=latest_market_data_timestamp["symbol"],
+                from_=latest_market_data_timestamp["timestamp"] + timedelta(days=1),
+            )
 
     log.info("Completed database update.")
